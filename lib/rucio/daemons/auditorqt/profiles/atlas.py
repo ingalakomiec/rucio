2e52fe89244f3ad2ae74707f5d8a82f7f167468c
ingalakomiec <inga.katarzyna.lakomiec@cern.ch>
2025-05-09 12:02:43 +0000

    auditor-qt: url for an RSE dump - preparation

--- lib/rucio/daemons/auditorqt/profiles/atlas.py
+++ lib/rucio/daemons/auditorqt/profiles/atlas.py
@@ -14,20 +14,67 @@
 
 """ATLAS-specific auditor profile."""
 
+import glob
+import hashlib
 import logging
 import os
+import re
 import requests
 import urllib.request
 
 from configparser import RawConfigParser
 from datetime import datetime, timedelta
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
-
+from rucio.common.config import get_config_dirs
+from rucio.common.constants import RseAttr
 from rucio.common.dumper import ddmendpoint_url
+from rucio.core.credential import get_signed_url
+from rucio.core.rse import get_rse_id, list_rse_attributes
+
 
 BASE_URL_RUCIO = 'https://eosatlas.cern.ch/eos/atlas/atlascerngroupdisk/data-adc/rucio-analytix/reports/{0}/replicas_per_rse/{1}*'
-#BASE_URL = '/user/rucio01/reports/{0}/replicas_per_rse/{1}'
+
+_DUMPERCONFIGDIRS = list(
+    filter(
+        os.path.exists,
+        (
+            os.path.join(confdir, 'auditor') for confdir in get_config_dirs()
+        )
+    )
+
+)
+
+OBJECTSTORE_NUM_TRIES = 30
+
+class Parser(RawConfigParser):
+    '''
+    RawConfigParser subclass that doesn't modify the the name of the options
+    and removes any quotes around the string values.
+    '''
+    remove_quotes_re = re.compile(r"^'(.+)'$")
+    remove_double_quotes_re = re.compile(r'^"(.+)"$')
+
+    def optionxform(
+            self,
+            optionstr: str
+    ) -> str:
+        return optionstr
+
+    def get(
+            self,
+            section: str,
+            option: str
+    ) -> Any:
+        value = super(Parser, self).get(section, option)
+        if isinstance(value, str):
+            value = self.remove_quotes_re.sub(r'\1', value)
+            value = self.remove_double_quotes_re.sub(r'\1', value)
+        return value
+
+    def items(self, section):
+        return [(name, self.get(section, name)) for name in self.options(section)]
+
 
 def atlas_auditor(
         nprocs: int,
@@ -65,23 +112,14 @@ def atlas_auditor(
 
     rucio_dump_after_path = '/opt/rucio/lib/rucio/daemons/auditorqt/tmp/real_dumps/rucio_dump_after/rucio_after.DESY-ZN_DATADISK_2025-01-30'
 
-#    configuration = parse_configuration()
-#    rse_dump_path_tmp, date_rse = fetch_rse_dump(rse, configuration, cache_dir)
+    configuration = parse_configuration()
 
-    rse_dump_path_tmp, date_rse = fetch_rse_dump(rse, cache_dir)
+    rse_dump_path_tmp, date_rse = fetch_rse_dump(rse, configuration, cache_dir, date)
 
 #    rucio_dump_before_path_tmp = fetch_rucio_dump(rse, date_rse - delta, cache_dir)
 #    rucio_dump_after_path_tmp = fetch_rucio_dump(rse, date_rse + delta, cache_dir)
 
     lost_files, dark_files = consistency_check(rucio_dump_before_path, rse_dump_path, rucio_dump_after_path)
-
-#    file_lost_files = open(results_dir+'/lost_files', 'w')
-#    file_lost_files.writelines(lost_files)
-#    file_lost_files.close()
-
-#    file_dark_files = open(results_dir+'/dark_files', 'w')
-#    file_dark_files.writelines(dark_files)
-#    file_dark_files.close()
 
     result_file_name = 'result.{0}_{1}'.format(
         rse,
@@ -102,61 +140,110 @@ def atlas_auditor(
 
     return True
 
-#def parse_configuration(conf_dirs: Optional[list[str]] = None) -> Parser:
-def parse_configuration(conf_dirs: Optional[list[str]] = None) -> None:
+def parse_configuration(conf_dirs: Optional[list[str]] = None) -> Parser:
 
-#    return configuration
-    return True
+    conf_dirs = conf_dirs or _DUMPERCONFIGDIRS
+    logger = logging.getLogger('auditor.parse_configuration')
+
+    if len(conf_dirs) == 0:
+        logger.error('No configuration directory given to load RSE dump path')
+        raise Exeption('No configuration directory given to load RSE dump path')
+        print("conf_dirs: ", conf_dirs)
+
+    configuration = Parser({
+        'disabled': False,
+    })
+
+    for conf_dir in conf_dirs:
+        configuration.read(glob.glob(conf_dir + '/*.cfg'))
+
+    return configuration
 
 def generate_url(
-    rse: str
-#    config: RawConfigParser
-#) -> tuple[str, str]:
+    rse: str,
+    config: RawConfigParser
 ) -> tuple[str, str]:
 
     print("generating url for rse")
 
     site = rse.split('_')[0]
 
-    print("site", site)
+    if site not in config.sections():
+        base_url = ddmendpoint_url(rse) + 'dumps'
+        url_pattern = 'dump_%Y%m%d'
 
-#sprawdzic, czy to site jest w pliku configuracyjnym wyszczegolnione, czyli dodac if ...
-
-    base_url = ddmendpoint_url(rse) + 'dumps'
-#    base_url = 'base_url_dump'
-    url_pattern = 'dump_%Y%m%d'
-
-# dodac if else, jak juz bedzie gotowy parametr configuration
+    else:
+        url_components = config.get(site, rse).split('/')
+        pattern_index = next(idx for idx, comp in enumerate(url_components) if '%m' in comp)
+        base_url = '/'.join(url_components[:pattern_index])
+        url_pattern = '/'.join(url_components[pattern_index:])
 
     return base_url, url_pattern
 
 def fetch_rse_dump(
     rse: str,
-    cache_dir: str
+    configuration: RawConfigParser,
+    cache_dir: str,
+    date: Optional[datetime] = None,
 ) -> tuple[str, datetime]:
 
     logger = logging.getLogger('auditor.fetch_rse_dump')
 
-
     print("fetching rse dump")
 
-    date_rse_dump = datetime.now()
+    base_url, url_pattern = generate_url(rse, configuration)
 
-    date_rse_dump = date_rse_dump.strftime('%Y-%m-%d')
+    print("base_url: ", base_url)
+
+    rse_id = get_rse_id(rse)
+    rse_attr = list_rse_attributes(rse_id)
+#    if RseAttr.IS_OBJECT_STORE in rse_attr and rse_attr[RseAttr.IS_OBJECT_STORE] is not False:
+    """
+    tries = 1
+    if date is None:
+        date = datetime.now()
+        tries = OBJECTSTORE_NUM_TRIES
+    path = ''
+    while tries > 0:
+        url = '{0}/{1}'.format(base_url, date.strftime(url_pattern))
+
+        filename = '{0}_{1}_{2}_{3}'.format(
+            'ddmendpoint',
+            rse,
+            date.strftime('%d-%m-%Y'),
+            hashlib.sha1(url.encode()).hexdigest()
+        )
+        filename = re.sub(r'\W', '-', filename)
+        path = os.path.join(cache_dir, filename)
+        if not os.path.exists(path):
+            logger.debug('Trying to download: "%s"', url)
+            if RseAttr.SIGN_URL in rse_attr:
+                url = get_signed_url(rse_id, rse_attr[RseAttr.SIGN_URL], 'read', url)
+                status_code = download (url, path)
+                if status_code != 200:
+                    tries -= 1
+                    date = date - timedelta(1)
+                else:
+                    tries = 0
+    """
+
+#                except
+#    url = '{0}/{1}'.format(base_url, date.strftime(url_pattern))
+#    print("url: ", url)
 
 
-#    base_url, url_pattern = generate_url(rse, configuration)
-    base_url, url_pattern = generate_url(rse)
-
+    if date is None:
+        logger.debug('Looking for site dumps in: "%s"', base_url)
+        links = get_links(base_url)
 
     filename_rse_dump = '{0}_{1}'.format(
         rse,
-        date_rse_dump
+        date
     )
 
     path_rse_dump = os.path.join(cache_dir, filename_rse_dump)
 
-    return (path_rse_dump, date_rse_dump)
+    return (path_rse_dump, date)
 
 def fetch_rucio_dump(
     rse: str,
@@ -169,12 +256,10 @@ def fetch_rucio_dump(
 
     date = date.strftime('%Y-%m-%d')
 
-#    url = BASE_URL.format(date,rse)
-#    url = ''.join((BASE_URL, '?date={0}&rse={1}'.format(date, rse)))
-    url = BASE_URL_RUCIO.format(date, rse)
+#    url = BASE_URL_RUCIO.format(date, rse)
 #    url = 'https://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/data-adc/rucio-analytix/reports/2025-05-04/replicas_per_rse/GOEGRID_TESTDATADISK.replicas_per_rse.2025-05-04.csv.bz2'
 
-#    url = 'https://learnpython.com/blog/python-pillow-module/1.jpg'
+    url = 'https://learnpython.com/blog/python-pillow-module/1.jpg'
 
     print('url:', url)
 
@@ -183,16 +268,23 @@ def fetch_rucio_dump(
         date
     )
 
-#    filename = re.sub(r'\W', '-', filename)
     path = os.path.join(cache_dir, filename)
 
-
-# te trzy linijki poten odkomentowac
-#    if os.path.exists(path):
-#        logger.debug('Taking Rucio Replica Dump %s for %s from cache', path, rse)
-#        return path
+    if os.path.exists(path):
+        logger.debug('Taking Rucio Replica Dump %s for %s from cache', path, rse)
+        return path
 
     logging.debug('Trying to download: %s for %s', url, rse)
+
+    status_code = download (url, path)
+
+    return path
+
+
+def download(
+    url: str,
+    path: str
+) -> int:
 
     response = requests.get(url, stream=True)
 
@@ -203,11 +295,10 @@ def fetch_rucio_dump(
         response.status_code,
         )
 
-    open(cache_dir+'/temporary.jpg', 'wb').write(response.content)
+    open(path, 'wb').write(response.content)
 
-#    urllib.request.urlretrieve(url, "/opt/rucio/lib/rucio/daemons/auditorqt/tmp/file_tmp.zip")
+    return response.status_code
 
-    return path
 
 def prepare_rse_dump(
     dump_path: str
