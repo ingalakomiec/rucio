0db323dabc8b29bbf705ceb89ea5ef338f149d46
ingalakomiec <inga.katarzyna.lakomiec@cern.ch>
2025-05-13 12:08:54 +0000

    auditor-qt: moving atlas specific parts to auditorqt/profiles/atlas_specific

--- lib/rucio/daemons/auditorqt/profiles/atlas_specific/__init__.py
+++ lib/rucio/daemons/auditorqt/profiles/atlas_specific/__init__.py
@@ -11,17 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Auditor profile definitions."""
-
-from .atlas import atlas_auditor
-from .atlasOld import atlas_auditor_old
-from .generic import generic_auditor
-
-PROFILE_MAP = {
-#    'generic_delete': generic_delete,
-#    'generic_move': generic_move,
-    'atlas': atlas_auditor,
-    'atlasOld': atlas_auditor_old,
-    'generic': generic_auditor
-}
