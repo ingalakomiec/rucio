e97f53bce96807e7ddb05155488574c833dd44e1
ingalakomiec <inga.katarzyna.lakomiec@cern.ch>
2026-07-02 12:18:22 +0000

    refactor(Consistency): add --compress-results argument to auditor-qt
    Issue: #3437

--- lib/rucio/daemons/auditorqt/dumps.py
+++ lib/rucio/daemons/auditorqt/dumps.py
@@ -12,42 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""perform actions on output of the auditor consistency check"""
+"""perform actions on dumps after the auditor consistency check"""
 
-import bz2
 import glob
 import logging
 import os
-
-def bz2_compress_file(
-        source_path: str,
-        chunk_size: int = 65000
-) -> str:
-
-    """Compress a file with bzip2.
-
-    The destination is the path passed through ``source`` extended with
-    '.bz2'.  The original file is deleted.
-
-    Errors are deliberately not handled gracefully.  Any exceptions
-    should be propagated to the caller.
-
-    ``source_path``: absolute path to the file to compress.
-
-    ``chunk_size``: size (in bytes) of the chunks by which to read the file.
-
-    Returns the destination path.
-    """
-
-    final_path = f"{source_path}.bz2"
-    with open(source_path) as plain, bz2.BZ2File(final_path, 'w') as compressed:
-        while True:
-            chunk = plain.read(chunk_size)
-            if not chunk:
-                break
-            compressed.write(chunk.encode())
-    os.remove(source_path)
-    return final_path
 
 
 def remove_cached_dumps(paths: []) -> bool:
@@ -55,7 +24,7 @@ def remove_cached_dumps(paths: []) -> bool:
     logger = logging.getLogger('auditor: output.remove_cached_dump')
 
     for path in paths:
-        #remove = glob.glob(f"{cache_dir}/*{rse}*")
+        # remove all dumps, also sorted and parsed
         remove = glob.glob(f"{path}*")
         for fil in remove:
             os.remove(fil)
